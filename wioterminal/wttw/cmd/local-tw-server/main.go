4e29dbe915f45db884ffe44d5e3e6f914352d68a
sago35 <sago35@gmail.com>
2020-07-13 22:31:55 +0900

    Update local-tw-server

--- wioterminal/wttw/cmd/local-tw-server/main.go
+++ wioterminal/wttw/cmd/local-tw-server/main.go
@@ -1,27 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	"github.com/sago35/tinygo-examples/wioterminal/wttw/tweet"
+)
+
+var (
+	index int
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, `
-    [
-        {
-            UserName: "Hiroshi SAKURAI",
-            ScreenName: "anolivetree",
-            CreatedAt: "Sat Jul 11 12:38:52 +0000 2020",
-            FullText: "TinyGoを調べてみたのだけれど、簡単な組み込み機器になら十分つかえそう。あとはこれに優先度順のスケジューラーを載せればTinyGoで良いというケースはかなりあると思う。",
-            FavoriteCount: 2,
-            RetweetCount: 0,
-            IsRetweet: false,
-        }
-    ]
-`)
+    t := tweet.S
+	b, err := json.Marshal(t[index])
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	log.Printf(r.URL.String())
+	fmt.Fprintf(w, string(b))
+	index = (index + 1) % len(t)
 }
+
+func handlerU(w http.ResponseWriter, r *http.Request) {
+    t := tweet.S
+    t[index].UserName = strings.ToUpper(t[index].UserName)
+	b, err := json.Marshal(t[index])
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	log.Printf(r.URL.String())
+	fmt.Fprintf(w, string(b))
+	index = (index + 1) % len(t)
+}
+
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/u", handlerU)
+
+	fmt.Printf("start server\n")
 	http.ListenAndServe(":8080", nil)
 }
