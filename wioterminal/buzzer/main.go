16191c2354b9f1cbf54b7631302dcb9ddfe0b513
sago35 <sago35@gmail.com>
2022-05-23 20:12:22 +0900

    Update ./wioterminal/buzzer/

--- wioterminal/buzzer/main.go
+++ wioterminal/buzzer/main.go
@@ -4,33 +4,41 @@ import (
 	"machine"
 	"time"
 
-	"tinygo.org/x/drivers/buzzer"
+	"tinygo.org/x/drivers/tone"
 )
-
-type note struct {
-	tone     float64
-	duration float64
-}
 
 func main() {
 	bzrPin := machine.WIO_BUZZER
-	bzrPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
-
-	bzr := buzzer.New(bzrPin)
-
-	song := []note{
-		{buzzer.C3, buzzer.Quarter},
-		{buzzer.D3, buzzer.Quarter},
-		{buzzer.E3, buzzer.Quarter},
-		{buzzer.F3, buzzer.Quarter},
-		{buzzer.G3, buzzer.Quarter},
-		{buzzer.A3, buzzer.Quarter},
-		{buzzer.B3, buzzer.Quarter},
-		{buzzer.C3, buzzer.Quarter},
+	pwm := machine.TCC0
+	speaker, err := tone.New(pwm, bzrPin)
+	if err != nil {
+		println("failed to configure PWM")
+		return
 	}
 
-	for _, val := range song {
-		bzr.Tone(val.tone, val.duration)
-		time.Sleep(10 * time.Millisecond)
+	song := []tone.Note{
+		tone.C5,
+		tone.D5,
+		tone.E5,
+		tone.F5,
+		tone.G5,
+		tone.A5,
+		tone.B5,
+		tone.C6,
+		tone.C6,
+		tone.B5,
+		tone.A5,
+		tone.G5,
+		tone.F5,
+		tone.E5,
+		tone.D5,
+		tone.C5,
+	}
+
+	for {
+		for _, val := range song {
+			speaker.SetNote(val)
+			time.Sleep(time.Second / 2)
+		}
 	}
 }
