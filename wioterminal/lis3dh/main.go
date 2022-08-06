38326599e0019daad513477c77c6fb45968bd43e
sago35 <sago35@gmail.com>
2022-08-06 10:14:53 +0900

    Updated to match the I2C definition in tinygo 0.24

--- wioterminal/lis3dh/main.go
+++ wioterminal/lis3dh/main.go
@@ -2,16 +2,17 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 	"time"
 
 	"tinygo.org/x/drivers/lis3dh"
 )
 
-var i2c = machine.I2C1
+var i2c = machine.I2C0
 
 func main() {
-	i2c.Configure(machine.I2CConfig{SCL: machine.SCL1_PIN, SDA: machine.SDA1_PIN})
+	i2c.Configure(machine.I2CConfig{SCL: machine.SCL0_PIN, SDA: machine.SDA0_PIN})
 
 	accel := lis3dh.New(i2c)
 	accel.Address = lis3dh.Address0 // address on the Wio Terminal
@@ -22,10 +23,10 @@ func main() {
 
 	for {
 		x, y, z, _ := accel.ReadAcceleration()
-		println("X:", x, "Y:", y, "Z:", z)
+		fmt.Printf("X: %-10d Y: %-10d Z: %-10d\r\n", x, y, z)
 
-		rx, ry, rz := accel.ReadRawAcceleration()
-		println("X (raw):", rx, "Y (raw):", ry, "Z (raw):", rz)
+		//rx, ry, rz := accel.ReadRawAcceleration()
+		//println("X (raw):", rx, "Y (raw):", ry, "Z (raw):", rz)
 
 		time.Sleep(time.Millisecond * 100)
 	}
